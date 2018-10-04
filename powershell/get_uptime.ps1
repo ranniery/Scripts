704218f46c959a75325d5f84782b6e31889e32b6
Ranniery Holanda <email@ranniery.com.br>
2018-10-04 12:11:44 -0300

    blablabla

---
+++ powershell/get_uptime.ps1
@@ -0,0 +1,32 @@
+﻿function Get-Uptime
+{
+    param(
+    [String[]]
+    $ComputerName
+    )
+    process {
+
+    if($ComputerName)
+        {
+        	$OperatingSystem = Get-WmiObject Win32_OperatingSystem -ComputerName $ComputerName
+        }
+    else
+        {
+            $OperatingSystem = Get-WmiObject Win32_OperatingSystem
+        }
+    
+    $LastBoot = $OperatingSystem.ConvertToDateTime($OperatingSystem.LastBootupTime)
+
+    $UpTime = (get-date).subtract(
+            $OperatingSystem.ConvertToDateTime(
+            $OperatingSystem.LastBootUpTime)
+        )
+
+    "Last boot: {0}" -f $LastBoot
+    "Uptime: {0}d {1}h {2}m {3}s" -f 
+        $UpTime.days, 
+        $UpTime.hours, 
+        $UpTime.minutes, 
+        $UpTime.seconds
+    }
+}
